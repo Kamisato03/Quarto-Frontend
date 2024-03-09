8f0121b6b6567fe1d4111248c156a936d4eda6ca
KgnavarroR <karlanavarro574@gmail.com>
2024-03-09 16:45:54 -0600

    feat de login usuarios

---
+++ src/pages/loginPage.vue
@@ -0,0 +1,190 @@
+<template>
+  <div>
+    <img src="../assets/logoRosa.svg" class="logoRosa" />
+  </div>
+  <div class="line"></div>
+  <h4 class="title-login">Inicia sesión</h4>
+  <q-form>
+    <div class="input-datos">
+      <p class="subtitul">Correo electrónico</p>
+      <q-input
+        outlined
+        class="input-login"
+        v-model="email"
+        bg-color="white"
+        style="font-size: 1.1rem; border: #635c5c"
+        dense
+        placeholder="Ingresa tu correo electrónico"
+        :rules="[
+          (val) => !!val || 'Este campo es requerido',
+          (val) =>
+            /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/.test(val) ||
+            'Formato incorrecto de email',
+        ]"
+      ></q-input>
+    </div>
+    <div class="input-datos">
+      <p class="subtitul">Contraseña</p>
+      <q-input
+        outlined
+        class="input-login"
+        v-model="password"
+        bg-color="white"
+        style="font-size: 1.1rem; border: #635c5c"
+        dense
+        placeholder="Ingresa tu contraseña"
+        :rules="[
+          (val) => {
+            if (val && val.length <= 5) {
+              return 'La contraseña debe tener mínimo 6 caracteres';
+            }
+            return true;
+          },
+        ]"
+        hide-bottom-space
+        :type="showPassword ? 'text' : 'password'"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="showPassword ? 'remove_red_eye' : 'visibility_off'"
+            class="cursor-pointer"
+            style="color: #635c5c"
+            @click="showPassword = !showPassword"
+          />
+        </template>
+      </q-input>
+    </div>
+
+    <div class="check-rec">
+      <div class="q-gutter-sm">
+        <q-checkbox v-model="right" label="Recordarme" color="cyan" />
+      </div>
+    </div>
+    <q-btn type="submit" class="btn-entrar" label="Entrar" text-color="white" />
+  </q-form>
+  <div class="forgotPwd">
+    <a class="text-center text-secondary" style="text-decoration: none" href="">
+      <b>¿Olvidaste tu contraseña?</b></a
+    >
+  </div>
+  <q-btn class="btn-google">
+    <img
+      src="../assets/logogoogle.svg"
+      width="25"
+      height="30"
+      class="iconGoogleBtn"
+    />
+    <label for="">Iniciar con Google</label></q-btn
+  >
+  <div>
+    <b class="subtitulo">¿Aún no tienes cuenta? </b>
+    <a class="subtitulo1" href=""> <b>Regístrate</b></a>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+//variables a usar
+const email = ref("");
+const password = ref("");
+const showPassword = ref(false);
+const right = ref(false);
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Glacial+Indifference&display=swap");
+
+.glacial-font {
+  font-family: "Glacial Indifference", sans-serif;
+}
+
+.input-datos {
+  width: 90%;
+  margin-bottom: -15px;
+  padding-left: 35px;
+  color: #635c5c;
+  font-size: 1.2rem;
+}
+
+.input-login {
+  width: 102%;
+  margin-top: 10px;
+  margin-bottom: 11px;
+}
+
+.title-login {
+  font-size: 1.8rem;
+  margin-bottom: 35px;
+  color: #635c5c;
+  text-align: center;
+}
+
+.forgotPwd {
+  font-size: 1.1rem;
+  margin-bottom: 55px;
+  color: #5fbfb8;
+  text-align: center;
+}
+
+.btn-entrar {
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-transform: none;
+  color: white;
+  width: 83%;
+  margin-left: 36px;
+  background-color: #5fbfb8 !important;
+  margin-bottom: 30px;
+}
+
+.btn-google {
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-transform: none;
+  color: #5fbfb8;
+  width: 83%;
+  margin-left: 36px;
+  background-color: white !important;
+  border: solid 1.2px #5fbfb8 !important;
+  margin-top: -25px;
+  margin-bottom: 30px;
+}
+
+.iconGoogleBtn {
+  margin-right: 12px;
+}
+
+.logoRosa {
+  width: 54%;
+  height: auto;
+  margin-left: 92px;
+  margin-top: 13px;
+}
+.subtitulo {
+  color: #797676;
+  font-size: 1.2rem;
+  font-weight: normal;
+  margin-left: 65px;
+}
+.subtitulo1 {
+  color: #f39daa;
+  font-size: 1.2rem;
+  margin-bottom: 30px;
+  font-weight: bold;
+  text-decoration: none;
+}
+
+.check-rec {
+  margin-left: 27px;
+  margin-bottom: 20px;
+  font-size: 1rem;
+  color: #797676;
+}
+
+.line {
+  width: 70%;
+  border-bottom: 2px solid #e3d2c6;
+  margin: 20px auto;
+}
+</style>
